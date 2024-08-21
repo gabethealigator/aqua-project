b221c08430da6b120ed7e760e2aacbd51ce69b2d
Leonardo Pacini <leopacskate@gmail.com>
2024-08-21 20:02:45 -0300

    [Update 5] Base HTML file on 'templates' creation / Authentication page and path to it creation / 'Entrar' and 'Registrar' buttons redirection

--- aquasite/views.py
+++ aquasite/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'aquasite/pages/home.html')
+
+class userAuth:
+  def auth(request):
+    return render(request, 'aquasite/pages/auth.html')
