8c6331e6a8f6558ee41a5d5310d1e9c6cad48811
Leonardo Pacini <leopacskate@gmail.com>
2024-08-21 22:01:50 -0300

    [Update 7] Link django with firebase implementation / Confidencial informations protection

--- aquasite/views.py
+++ aquasite/views.py
@@ -1,4 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from decouple import config
+
+import pyrebase
+
+firebaseConfig = {
+  'apiKey': config('FIREBASE_API_KEY'),
+  'authDomain': config('FIREBASE_AUTH_DOMAIN'),
+  'databaseURL': config('FIREBASE_DATABASE_URL'),
+  'projectId': config('FIREBASE_PROJECT_ID'),
+  'storageBucket': config('FIREBASE_STORAGE_BUCKET'),
+  'messagingSenderId': config('FIREBASE_MESSAGING_SENDER_ID'),
+  'appId': config('FIREBASE_APP_ID')
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+database = firebase.database()
+auth = firebase.auth()
 
 def home(request):
   return render(request, 'aquasite/pages/home.html')
@@ -6,9 +24,6 @@ def home(request):
 class userAuth:
   def auth(request):
     return render(request, 'aquasite/pages/auth.html')
-  
-  def login(request):
-    return
   
 def dashboard(request):
   return render(request, 'aquasite/pages/dashboard.html')
