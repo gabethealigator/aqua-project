72805619bc47dba800f94723912c78cacde2c1d3
Leonardo Pacini <leopacskate@gmail.com>
2024-09-02 21:50:21 -0300

    [Update 32] fontawesomefree folder orgnanization / Dark theme homepage addition / Dark theme dahsboard addition / Buttons to switch theme on dashboard creation / Buttons to switch theme on homepage creation / Script to switch theme creation

--- aquaproject/settings.py
+++ aquaproject/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'aquasite',
-    'bootstrap5',
     'fontawesomefree',
     'channels',
 ]
---
+++ aquasite/static/aquasite/js/switch-theme.js
@@ -0,0 +1,47 @@
+const page = document.querySelector('[data-bs-theme]')
+const dashBtns = document.getElementsByName('options-outlined')
+const homeBtns = document.getElementsByName('theme-btn')
+
+if (localStorage.getItem('mode') == 'dark') {
+  setDarkMode()
+} else {
+  setLightMode()
+}
+
+function setDarkMode() {
+  localStorage.setItem('mode', 'dark')
+  page.dataset.bsTheme = 'dark'
+
+  if (dashBtns.length !== 0) {
+    dashBtns[0].checked = false
+    dashBtns[1].checked = true
+  } else if (homeBtns.length !== 0) {
+    homeBtns[0].style.display = 'none'
+    homeBtns[1].style.display = 'block'
+  }
+}
+
+function setLightMode() {
+  localStorage.setItem('mode', 'light')
+  page.dataset.bsTheme = 'light'
+
+  if (dashBtns.length !== 0) {
+    dashBtns[0].checked = true
+    dashBtns[1].checked = false
+  } else if (homeBtns.length !== 0) {
+    homeBtns[0].style.display = 'block'
+    homeBtns[1].style.display = 'none'
+  }
+}
+
+for (let i = 0; i < homeBtns.length; i++) {
+  homeBtns[i].addEventListener('click', ()=> {
+    console.log(homeBtns[i].id === 'switch-light-theme')
+
+    if (homeBtns[i].id === 'switch-light-theme') {
+      setDarkMode()
+    } else {
+      setLightMode()
+    }
+  })
+}
